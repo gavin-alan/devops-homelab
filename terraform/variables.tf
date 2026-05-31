9d4f490e0b46369d01e366bb9d4ca6f8e5ddb634
gavin-a-admin <git@gavinalan.com>
2026-05-31 16:08:37 -0400

    phase 1: terraform config with VPC, subnet, IGW, SG, key pair, and EC2

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+﻿variable "aws_region" {
+  description = "AWS region to deploy into"
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t3.micro"
+}
+
+variable "project_name" {
+  description = "Project name used for tagging"
+  default     = "devops-homelab"
+}
